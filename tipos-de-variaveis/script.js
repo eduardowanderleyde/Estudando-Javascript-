17a4108d788e71633631477ccb1222788fcbaef8
Eduardo Wanderley de Siqueira Araújo <103546791+eduardowanderleyde@users.noreply.github.com>
2022-09-28 00:34:25 -0300

    estudando variaveis

---
+++ tipos-de-variaveis/script.js
@@ -0,0 +1,11 @@
+var nome='Pablo'
+let idade=25
+const aprovado = true
+//tipos de variaveis//
+// boolean = true e false
+//null = Nulo
+// undefined = não definido
+// number = 42 ou 3.14159
+// string = "Howdy
+
+//NAO UTILIZAR O VAR E SIM O LET//
